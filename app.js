5f7f87a0b34b32983d97d76f879f89b0f0c61351
teppeis <teppeis@gmail.com>
2014-11-13 00:28:16 +0900

    Remove sample users

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var csp = require('helmet-csp');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -31,7 +30,6 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
